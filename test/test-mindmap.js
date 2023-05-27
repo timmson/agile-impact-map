002b8bf746ab52b177b1d72c2d259cd01794908d
Artem Krotov <timmson666@mail.ru>
2023-05-27 08:40:20 +0300

    - Some refactoring (#23)

--- test/test-mindmap.js
+++ test/test-mindmap.js
@@ -1,4 +1,4 @@
-const MindMap = require("../src/mindmap");
+const MindMap = require("../src/mindmap")
 const NodesAndLinks = {
 	nodes: [
 		{
@@ -23,20 +23,20 @@ const NodesAndLinks = {
 		}
 	]
 
-};
+}
 
 describe("Map should", () => {
 
-	const mindMap = new MindMap(NodesAndLinks);
+	const mindMap = new MindMap(NodesAndLinks)
 
 	test("transform source to nodes and links", () => {
 
-		const map = mindMap.getMap();
+		const map = mindMap.getMap()
 
-		expect(map.nodes).toHaveLength(2);
-		expect(map.links).toHaveLength(1);
-		expect(map.nodes[0].color).toEqual("#7fa23e");
-		expect(map.links[0].color).toEqual("#939393");
-	});
+		expect(map.nodes).toHaveLength(2)
+		expect(map.links).toHaveLength(1)
+		expect(map.nodes[0].color).toEqual("#7fa23e")
+		expect(map.links[0].color).toEqual("#939393")
+	})
 
-});
+})
